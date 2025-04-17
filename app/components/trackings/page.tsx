6d4848bab9b5217b5cbb8aae5036880e8db4e9a6
Your Name <your-email@example.com>
2025-04-17 20:13:40 +0100

    updated file

--- app/components/trackings/page.tsx
+++ app/components/trackings/page.tsx
@@ -1,10 +1,10 @@
 'use client'; 
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import TopBar from "../components/TopBar";
-import Sidebar from "../components/Sidebar";
+import TopBar from "../TopBar";
+import Sidebar from "../Sidebar";
 
-const MyMapComponent = dynamic(() => import("../components/trackings/tracking-map/TrackingMap"), { ssr: false });
+const MyMapComponent = dynamic(() => import("../trackings/tracking-map/TrackingMap"), { ssr: false });
 
 export default function TrackingPage() {
   const [location, setLocation] = useState<{ lat: number; lng: number } | null>(null);
